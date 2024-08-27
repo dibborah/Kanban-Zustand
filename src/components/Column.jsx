4e32643dfc972357429f90d1e53399bd491fb307
dibya.borah <dibyajyotiborah469@gmail.com>
2024-08-27 12:56:03 +0530

    displaying task from store

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -4,14 +4,19 @@ import Task from './Task';
 
 // eslint-disable-next-line react/prop-types
 const Column = ({ state }) => {
-    const tasks = useStore((store) => store.tasks);
-
+    const tasks = useStore(
+        (store) => store.tasks.filter(task => task.state === state)
+    );
     return (
         <div className="column">
             <p>{state}</p>
-            <Task title="Todo" />
+            {tasks.map((task) => {
+                return <Task title={task.title} key={Math.random()} />// generating new keys on the fly should be strictly avoided // I am doinng this since this will map a very small array
+            })}
         </div>
     )
 }
 
 export default Column;
+
+// const filtered = useMemo(() => tasks.filter(task => task.state === state), [tasks, state]);
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,16 +1,17 @@
 import classNames from 'classnames';
 import './Task.css';
-
-const STATUS = 'ONGOING';
+import { useStore } from '../store';
 
 // eslint-disable-next-line react/prop-types
 const Task = ({ title }) => {
+    const task = useStore((store) => store.tasks.find((task) => task.title == title));
+
     return (
         <div className="task">
-            <div>{title}</div>
+            <div>{task.title}</div>
             <div className='bottomWrapper'>
                 <div></div>
-                <div className={classNames('status', STATUS)}>{STATUS}</div>
+                <div className={classNames('status', task.state)}>{task.state}</div>
             </div>
         </div>
     )
